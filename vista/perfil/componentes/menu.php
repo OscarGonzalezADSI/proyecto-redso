7499004a98944a51b0ba115c71d86973ce0596d8
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2021-12-06 15:59:46 -0500

    Add files via upload

---
+++ vista/perfil/componentes/menu.php
@@ -0,0 +1,43 @@
+<!-- Accordion -->
+<div class="w3-card w3-round">
+    <div class="w3-white">
+        <button onclick="myFunction('Demo1')" class="w3-button w3-block w3-theme-l1 w3-left-align">
+            <i class="fa fa-circle-o-notch fa-fw w3-margin-right"></i> My Groups
+        </button>
+        <div id="Demo1" class="w3-hide w3-container">
+            <p><a href="../analiticas/analiticas.php">Analiticas</a></p>
+        </div>
+        <button onclick="myFunction('Demo2')" class="w3-button w3-block w3-theme-l1 w3-left-align">
+            <i class="fa fa-calendar-check-o fa-fw w3-margin-right"></i> My Events
+        </button>
+        <div id="Demo2" class="w3-hide w3-container">
+            <p>Some other text..</p>
+        </div>
+        <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align">
+            <i class="fa fa-users fa-fw w3-margin-right"></i> My Photos
+        </button>
+        <div id="Demo3" class="w3-hide w3-container">
+            <div class="w3-row-padding">
+                <br>
+                <div class="w3-half">
+                    <img src="../../images/lights.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+                <div class="w3-half">
+                    <img src="../../images/nature.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+                <div class="w3-half">
+                    <img src="../../images/mountains.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+                <div class="w3-half">
+                    <img src="../../images/forest.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+                <div class="w3-half">
+                    <img src="../../images/nature.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+                <div class="w3-half">
+                    <img src="./../images/snow.jpg" style="width:100%" class="w3-margin-bottom">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
